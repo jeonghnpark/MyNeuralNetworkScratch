682a7282c685bf5c0e46ef138947ba82a90aab65
jeonghnpark <jeonghn.park@gmail.com>
2021-07-18 18:01:46 +0900

    predict(network, x):

--- ch3.py
+++ ch3.py
@@ -21,30 +21,29 @@ def img_show(img):
 
 
 img = x_train[0]
-img=img.reshape(28,28)
+img = img.reshape(28, 28)
 img_show(img)
+
 
 def init_network():
     with open('sample_weight.pkl', 'rb') as f:
-        network=pickle.load(f)
+        network = pickle.load(f)
     return network
 
-network=init_network()
+
+network = init_network()
 print(network['W1'].shape)
 
 
 def predict(network, x):
-    W1, W2, W3=network['W1'], network['W2'], network['W3']
-    b1, b2, b3=network['b1'],network['b2'],network['b3']
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-    a1=np.dot(x,W1)+b1
-    z1=sigmoid(a1)
-    a2=np.dot(z1, W2)+b2
-    z2=sigmoid(a2)
-    a3=np.dot(a2, W3)+b3
-    y=softmax(a3)
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(a2, W3) + b3
+    y = softmax(a3)
 
     return y
-
-
-
